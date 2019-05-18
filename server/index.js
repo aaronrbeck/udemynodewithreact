c5229f46f1037f4b3e85895e8495bd2ad089ab53
Aaron <aaronrbeck@gmail.com>
2019-05-17 18:05:03 -0600

    lesson 28 complete - sorta finished with authentication

--- server/index.js
+++ server/index.js
@@ -26,8 +26,10 @@ passport.use(new GoogleStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, 
-(accessToken) =>{
-    console.log(accessToken)
+(accessToken, refreshToken, profile, done) =>{
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile:', profile);
 }
 )
 );
@@ -39,12 +41,15 @@ passport.use(new GoogleStrategy({
 //scope: ['profile', 'email'] is an options object which specifies which access we want to have
 //in our case we want access to profile and email.  profile and email are google predefined things
 //other options could include asking access to user's contact list or all images in drive account, etc.
+
+//lesson 26ish: user visits our auth/google url then google asks them if they want to grant us access to their info
 app.get(
     '/auth/google',
     passport.authenticate('google', {
     scope: ['profile', 'email']
 })
 )
+//lesson 27 after user grants the passport request with the token they grabbed from google we authenticate with that token
 app.get('/auth/google/callback', passport.authenticate('google'))
 
 
