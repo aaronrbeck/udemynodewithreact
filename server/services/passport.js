6fa2d015a4d73b329e074301890180a535d33a0a
Aaron <aaronrbeck@gmail.com>
2019-05-20 09:36:33 -0600

    lesson 39 added to passport.js so that we could save a new record to mongodb

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,9 +1,10 @@
 //bring in passport
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
 
-
+const User = mongoose.model('users')
 
 //passport.use - general strategy
 
@@ -13,9 +14,7 @@ passport.use(new GoogleStrategy({
     callbackURL: '/auth/google/callback'
 },
     (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile:', profile);
+        new User ({googleId: profile.id }).save()
     }
 )
 );
