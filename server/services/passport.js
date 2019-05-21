9305dd6fc62be71ff272e1e507a868a39cfd8658
Aaron <aaronrbeck@gmail.com>
2019-05-21 09:25:14 -0600

    for Bobby, token error check, thanks mate

--- server/services/passport.js
+++ server/services/passport.js
@@ -36,13 +36,13 @@ passport.use(new GoogleStrategy({
 },
     async (accessToken, refreshToken, profile, done) => {
        const existingUser = await User.findOne({ googleId: profile.id })
-           if(existingUser){
-            return done(null, existingUser)
-            }
-               const user = await new User({ googleId: profile.id }).save()
-                done(null, user)
-
-            
-    }
-)
+           
+       if(existingUser){
+        return done(null, existingUser)
+        }
+        
+        const user = await new User({ googleId: profile.id }).save()
+        done(null, user)
+        }
+    )
 );
