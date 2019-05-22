f6933111c86fb2136ed4c7e23a96e2fc1d5d94d3
Aaron <aaronrbeck@gmail.com>
2019-05-22 09:13:46 -0600

    109 - added niave method to check to see if someone is logged in before adding credits

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -3,6 +3,11 @@ const stripe = require ('stripe')(keys.stripeSecretKey)
 
 module.exports = app => {
     app.post('/api/stripe', async (req, res) =>{
+        //check to see if a user is signed in
+        if (!req.user){
+            return res.status(401).send({ error: 'you must be logged in to add credits'})
+        }
+
         const charge = await stripe.charges.create({
             //logic to handle the strip token
             //specify on back-end charge amount in us cents
