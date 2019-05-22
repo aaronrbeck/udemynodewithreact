c6f6e074b2519cb20b0aae150132fbe8db5f7865
Aaron <aaronrbeck@gmail.com>
2019-05-22 08:57:38 -0600

    106 - changed billingRoutes.js to handle stripe.charges on the back end

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,9 +1,18 @@
 const keys = require('../config/keys')
-const strip = require ('stripe)'(keys.stripeSecretKey)
+const stripe = require ('stripe')(keys.stripeSecretKey)
 
 module.exports = app => {
-    app.post('/api/strip', (req, res) =>{
-        //logic to handle the strip token
+    app.post('/api/stripe', (req, res) =>{
+        stripe.charges.create({
+            //logic to handle the strip token
+            //specify on back-end charge amount in us cents
+            amount: 500,
+            currency: 'usd',
+            description: '$5 for 5 credits',
+            source: req.body.id
+            
+
+        })
 
         //update user number of credits
 
