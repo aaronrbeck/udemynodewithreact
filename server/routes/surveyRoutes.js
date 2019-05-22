91f7b6c26cd003e8918764d2eab7d8b6b51123d8
Aaron <aaronrbeck@gmail.com>
2019-05-22 17:20:10 -0600

    126 - create middleware to check if user has enough credits to make a survey

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,11 +1,10 @@
 const requireLogin = require('../middlewares/requireLogin')
-
+const requireCredits = require('../middlewares/requireCredits')
 
 module.exports = app =>{
-    app.post('/api/surveys', requireLogin, (req, res) => {
-        //check to see if user is logged in
-
-        //check to make sure they have enough credits to make a new survey
+    app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+        //check to see if user is logged in accomplished with requireLogin parameter
+        //check to make sure they have enough credits to make a new survey accomplished with requireCredits parameter
         
     })
 }
