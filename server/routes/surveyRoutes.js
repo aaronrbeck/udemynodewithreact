003f0adfc8ac18c6819aada3c9ee5e3223298bc3
Aaron <aaronrbeck@gmail.com>
2019-05-22 17:26:41 -0600

    127 - started setting up a new survey instance in surveyRoutes.js

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,10 +1,26 @@
+const mongoose = require('mongoose')
+
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
+
+
+//somehow the following line allows us to side-step testing?  Why would we want to side-step future testing?
+const Survey = mongoose.model('surveys')
 
 module.exports = app =>{
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
         //check to see if user is logged in accomplished with requireLogin parameter
         //check to make sure they have enough credits to make a new survey accomplished with requireCredits parameter
-        
+        //
+        const {title, subject, body, recipients } = req.body
+        //more common in real world to do back end design before front end design
+
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            
+        })
+    
     })
 }
