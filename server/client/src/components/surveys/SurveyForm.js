5290d3c1a56a0108a86e975b6639cad051d34b9d
Aaron <aaronrbeck@gmail.com>
2019-05-23 11:25:17 -0600

    152 - destructured Field from redux-form, added Field form to surveyform

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -1,7 +1,7 @@
 //survey form shows a form for user to add inpu
 
 import React, { Component } from 'react'
-import { reduxForm } from 'redux-form'
+import { reduxForm, Field } from 'redux-form'
 
 
 //reduxForm allows our component to communicate with the store at the top of our application
@@ -12,7 +12,21 @@ class SurveyForm extends Component {
     render() {
         return (
             <div>
-                hi from survey form
+                {/* Field is useless with out props */}
+                <form onSubmit={this.props.handleSubmit(values => console.log(values))}
+                >
+                <Field
+                //three required properties for a reduxForm field element:
+                type="text"
+                // redux field will take the inputed value and store it in our redux
+                // store with a key of name (surveyTitle in this case)
+                name="surveyTitle"
+                //identify that we want an html input field of type text
+                component="input"
+                value="surveyTitle"
+                />
+                <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
