aff33845c9c91bb43a77b23dfbe6afe7c897884f
Aaron <aaronrbeck@gmail.com>
2019-05-23 10:34:19 -0600

    146 - used react router dom  in dashboard to navigate to /new

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+
 
 const Dashboard = () => {
     return (
         <div>
             hi from dashboad.js
             <div className ="fixed-action-btn">
-            <a className="btn-floating btn-large red">
+                {/* anchor tags not allowed in components
+                so import in and use Link from reac-router-dom */}
+            <Link to="/surveys/new" className="btn-floating btn-large red">
                 <i className="material-icons">add</i>
-            </a>
+            </Link>
             </div>
         </div>
     )
