b0a713031412266163aba237a59ebfee00753cd0
Aaron <aaronrbeck@gmail.com>
2019-05-24 10:28:52 -0600

    176 - set up local tunnel in start scripts

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -8,15 +8,16 @@ export const fetchUser = () => async dispatch => {
     }
 
 
-    //lesson 172 - there is some strange incongruity between my action creators and the instructor's action creators - not sure how this is happening
+export const handleToken = (token) => async dispatch => {
+    const res = await axios.post('/api/stripe', token)
+    dispatch({ type: FETCH_USER, payload: res.data })
+   
+}
+
+//lesson 172 - there is some strange incongruity between my action creators and the instructor's action creators - not sure how this is happening
 export const submitSurvey = (values, history) => async dispatch => {
     const res = await axios.post('/api/surveys', values)
     history.push('/surveys')
     dispatch({ type: FETCH_USER, payload: res.data })
 
-}
-export const handleToken = (token) => async dispatch => {
-    const res = await axios.post('/api/stripe', token)
-    dispatch({ type: FETCH_USER, payload: res.data })
-   
 }
