ec18c0b6821c143d4f68a23b8549d4d69a703266
Aaron <aaronrbeck@gmail.com>
2019-05-24 17:11:01 -0600

    198 - added to react/src/actions/types.js and corresponding action creator in src/actions.index.js and reducer in src/reducers/surveysReducer.js then wire that to reducers/index.js

--- server/client/src/reducers/index.js
+++ server/client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux'
 //import redux reducer:
 import { reducer as reduxForm } from 'redux-form'
 import authReducer from './authReducer'
+import surveysReducer from './surveysReducer'
 
 export default combineReducers({
     //add reducer as k:v pair, reducer's output will be stored to its key
     //in state object maintained by redux
     auth: authReducer,
-    form: reduxForm
+    form: reduxForm,
+    surveys: surveysReducer
 })
